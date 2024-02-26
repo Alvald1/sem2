63bb17d879fa2eec182ac8576eb42a143016950e
Kirill <kirill.a.gribov@gmail.com>
2024-02-26 22:53:50 +0300

    перезагрузить лаб1

--- 1/lib/code_status.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CODE_STATUS__
-#define __CODE_STATUS__
-
-enum { OK, BAD_ALLOC, BAD_FILE };
-
-enum { FMODE_ON, FMODE_OFF, FMODE_RES };
-
-#endif
--- 1/lib/task.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TASK__
-#define __TASK__
-
-#include "matrix.h"
-
-int task(Matrix* matrix, Matrix* result);
-
-#endif
