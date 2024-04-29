f626ef5696550fa9c0ad454364ef94d78eff8960
Kirill <kirill.a.gribov@gmail.com>
2024-04-29 14:37:39 +0300

    написал table_init

--- 3b/lib/table.c
+++ 3b/lib/table.c
@@ -1,7 +1,29 @@
 #include "table.h"
 
-struct _table {
-    Item* items;
-    size_t capacity, size;
-    Info* info;
-} Table;
+#include <stdlib.h>
+
+#include "info_lib.h"
+#include "table_lib.h"
+
+Foo
+table_init(Table** table, size_t capacity, Info* info) {
+    if (table == NULL || __info_valid(info) == BAD_DATA) {
+        return BAD_DATA;
+    }
+    *table = (Table*)malloc(sizeof(Table));
+    if (*table == NULL) {
+        return BAD_ALLOC;
+    }
+    Item* items = (Item*)malloc(capacity * sizeof(Item));
+    if (items == NULL) {
+        return BAD_ALLOC;
+    }
+    (*table)->capacity = capacity;
+    (*table)->info = info;
+    (*table)->items = items;
+    (*table)->size = 0;
+    return OK;
+}
+
+Foo
+__tale_valid(Table* table) {}
