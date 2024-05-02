0892ab737e109d2ef362344dfc99a9955be26ab6
Kirill <kirill.a.gribov@gmail.com>
2024-05-02 11:36:38 +0300

    сообщения об ошибках

---
+++ 3b/lib/code_status.c
@@ -0,0 +1,4 @@
+#include "code_status.h"
+
+const char* errors[] = {"Good\n",        "Invalid data\n", "Memory error\n", "Overflow\n",
+                        "Not founded\n", "Invalid key\n",  "Invalid file\n", "Empty\n"};
